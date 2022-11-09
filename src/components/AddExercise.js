d29d1760c2f2e57d7f65a5feafbfefd63f7b7469
mortal Wombat <83345498+ThatOneJon@users.noreply.github.com>
2022-11-09 21:04:03 +0100

    ADD Exercise functionality

--- src/components/AddExercise.js
+++ src/components/AddExercise.js
@@ -7,6 +7,17 @@ import Form from 'react-bootstrap/Form'
 
 
 export default function AddExercise(){
+
+    const[formData, setFormData] = React.useState({
+        quality: ""
+    })
+
+
+    function handleChange(event){
+        setFormData({quality : event.target.value})
+        const thisId = event.target.value
+    }
+
     return(
         <Container>
             <Form>
@@ -17,16 +28,16 @@ export default function AddExercise(){
             <Row className="d-flex flex-row text-center">
                 <Col>
                     <Form.Group>
-                        <label className="radioImgLabel">
-                            <input type ="radio" style={{appearance:"none"}} name="smileyRadioButton" value="happy"/>
+                        <label className="radioImgLabel" id ="happy" style = {formData.quality === "happy" ? {border:"3px solid black", borderRadius:"10px", padding:"3px", backgroundColor:"lightgreen"}:{border: "none" }}>
+                            <input  type ="radio" style={{appearance:"none"}}  value="happy" checked={formData.quality ==="happy"} onChange={(event) => handleChange(event)} />
                             <img src = {require("./imgs/Happy.png")} style={{width:"9vw", borderRadius:"20px", maxWidth:"150px", minWidth:"80px"}}/>
                         </label>
                     </Form.Group>
                 </Col>
                 <Col>
                     <Form.Group>
-                    <label className="radioImgLabel">
-                            <input type ="radio" style={{appearance:"none"}} name="smileyRadioButton" value="sad" />
+                        <label className="radioImgLabel" id="sad" style = {formData.quality === "sad" ? {border:"3px solid black", borderRadius:"10px", padding:"3px", backgroundColor:"red"}:{border: "none" }}>
+                            <input type ="radio" style={{appearance:"none"}}  value="sad" checked={formData.quality === "sad"} onChange={(event) => handleChange(event)} />
                             <img src = {require("./imgs/sad.png")} style={{width:"9vw", borderRadius:"20px", maxWidth:"150px", minWidth:"80px"}}/>
                         </label>
                     </Form.Group>

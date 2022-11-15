f919d45a9f41c1ebaa51718e2846571345d92289
mortal Wombat <83345498+ThatOneJon@users.noreply.github.com>
2022-11-15 13:03:24 +0100

    login Token imp

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,25 +10,44 @@ import AuthContext from '../context/authContext';
 
 export default function Login(){
 
-    let {name} = useContext(AuthContext)
+    let {loginUser} = useContext(AuthContext)
+    //getting the login user function through destructuring context (import useContext Hook) --> Then onSubmit of form -> loginUser()
+    let [loginData, setLoginData] = React.useState({
+        'userName': "",
+        'password': ""
+    })
+
+    function handleChange(event){
+        event.target.name === "userName" ?
+        setLoginData(prev => ({
+            ...prev,
+            'userName': event.target.value
+        }))
+        :
+        setLoginData(prev => ({
+            ...prev,
+            'password':event.target.value
+        }))
+    }
+    console.log(loginData)
 
     return(
         <Container fluid="lg" className="mt-5 mb-4">
             <Row className="justify-content-center">
                 <Col lg ={5}>
                     <h1 className = "formH1">Login</h1>
-                    <Form>
+                    <Form onSubmit = {loginUser}>
                         <Form.Group>
                             <Form.Label className="mt-3 customLabel">Username</Form.Label>
                             <InputGroup>
                                 <InputGroup.Text>@</InputGroup.Text>
-                                <Form.Control size = "lg" type="text" placeholder="Username" style={{backgroundColor: "#edeef7"}}/>
+                                <Form.Control size = "lg" type="text" placeholder="Username" style={{backgroundColor: "#edeef7"}} onChange={(event) => handleChange(event)} value = {loginData.userName} name ="userName"/>
                             </InputGroup>
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formLoginPassword">
+                        <Form.Group className="mb-3" controlId="formLoginPassword" name ="password">
                             <Form.Label className="mt-3 customLabel">Password</Form.Label>
-                            <Form.Control size = "lg" type="password" placeholder="Password" style={{backgroundColor: "#edeef7"}}/>
+                            <Form.Control size = "lg" type="password" placeholder="Password" style={{backgroundColor: "#edeef7"}} onChange={(event) => handleChange(event)} value={loginData.password}/>
                         </Form.Group>
                         <Button variant="dark" type="submit" className="btn-lg">
                             Submit
